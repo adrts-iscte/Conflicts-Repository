f0719fe0b9fa9ac11d1619bc3c7bb6e98e2c34d8
adrts-iscte <adrts@iscte-iul.pt>
2022-06-30 10:21:55 +0000

    Updates on ConflictingClass

--- src/ConflictingClass.java
+++ src/ConflictingClass.java
@@ -1,14 +1,17 @@
 public class ConflictingClass {
 
     void x() {
+        char x = 'x';
         System.out.println("x");
     }
 
     void y() {
+        char y = 'y';
         System.out.println("y");
     }
 
     void z() {
+        char z = 'z';
         System.out.println("z");
     }
 
