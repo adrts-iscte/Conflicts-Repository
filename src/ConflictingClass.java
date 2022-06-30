8d361609f3335a97c89234ead9ba1330e17a605e
adrts-iscte <adrts@iscte-iul.pt>
2022-06-30 10:25:43 +0000

    Change positions of two functions inside same class

--- src/ConflictingClass.java
+++ src/ConflictingClass.java
@@ -1,18 +1,18 @@
 public class ConflictingClass {
 
-    void x() {
-        int x = 88;
-        System.out.println("x");
+    void z() {
+        char z = 'z';
+        System.out.println("z");
     }
 
     void y() {
         char y = 'y';
         System.out.println("y");
     }
-    
-    void z() {
-        char z = 'z';
-        System.out.println("z");
+
+    void x() {
+        int x = 88;
+        System.out.println("x");
     }
 
 }
