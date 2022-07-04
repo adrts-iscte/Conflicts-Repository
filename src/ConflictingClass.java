4694cdf7e28714ad9ffe5aa5ef089b0c13ef6bc2
adrts-iscte <58140205+adrts-iscte@users.noreply.github.com>
2022-07-04 22:56:49 +0000

    Update ConflictingClass.java

--- src/ConflictingClass.java
+++ src/ConflictingClass.java
@@ -3,6 +3,9 @@ public class ConflictingClass {
     void x() {
         int x = 88;
         System.out.println("x");
+        for (int i = 0; i != 5; i++) {
+            System.out.println(i);
+        }
     }
 
     void y() {
