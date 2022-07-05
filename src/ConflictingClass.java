c878829f2c376e87e74d806bf332ca975a75205c
adrts-iscte <58140205+adrts-iscte@users.noreply.github.com>
2022-07-05 09:17:59 +0000

    Update ConflictingClass.java

--- src/ConflictingClass.java
+++ src/ConflictingClass.java
@@ -8,14 +8,14 @@ public class ConflictingClass {
         }
     }
 
-    void z() {
-        char z = 'z';
-        System.out.println("z");
-    }
-
     void y() {
         char y = 'y';
         System.out.println("y");
     }
 
+    void z() {
+        char z = 'z';
+        System.out.println("z");
+    }
+    
 }
